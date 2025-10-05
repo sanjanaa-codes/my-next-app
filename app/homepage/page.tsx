f5c21b9c3c080ac9d741913d04398b3b27b1ab7d
R J Sanjanaa <sanjanaa0727@gmail.com>
2025-10-05 17:35:16 +0530

    Update page.tsx

--- app/homepage/page.tsx
+++ app/homepage/page.tsx
@@ -1,13 +1,9 @@
-// import Header from "../components/header";
-
 import ProductCard from "../components/productCard";
 
 export default function HomePage() {
   return (
     <>
-      <h1>Hello, I am Home Page!</h1>
       <ProductCard />
-      {/* <Header /> */}
     </>
   );
 }
