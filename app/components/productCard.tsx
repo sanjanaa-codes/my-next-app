650725da13671f23b31c58c719c38647cac9486a
R J Sanjanaa <sanjanaa0727@gmail.com>
2025-10-05 17:35:20 +0530

    Update productCard.tsx

--- app/components/productCard.tsx
+++ app/components/productCard.tsx
@@ -1,26 +1,48 @@
-'use client';
+"use client";
+
+import StarIcon from "@/public/icons/star";
 import Image from "next/image";
-import { useState } from "react";
 
 export default function ProductCard() {
-  const [name, setName] = useState("Sanjanaa");
   return (
-    <div>
-      <Image
-        src="/images/imagecardbanner.jpg"
-        alt="Product Image"
-        width={200}
-        height={200}
-      />
-      <h2>Image Card Banner</h2>
-      <p>$19.99</p>
-
-      <button
-        className="bg-blue-500 text-white px-4 py-2 rounded cursor-pointer"
-        onClick={() => setName("saanj")}
-      >
-        Click Me {name}
-      </button>
+    <div className="grid grid-cols-3 gap-8 px-12 mt-20">
+      {Array(9)
+        .fill(0)
+        .map((_, i) => (
+          <div key={i}>
+            <Image
+              key={i}
+              src="/images/imagecardbanner.jpg"
+              alt="Product Image"
+              width={200}
+              height={200}
+              className="w-full h-auto cursor-pointer"
+            />
+            <div>
+              <h5 className="text-base font-normal mt-2">RE Weather Jacket</h5>
+              <p className="text-gray-600 truncate text-base font-medium mt-1">
+                Prestige Omega Select Plus 27.5cm Non-Stick Omni Tawa |Induction
+                Base |Gas & Induction Compatible| Metal Spoon Friendly | Sturdy
+                Handles
+              </p>
+              <div className="flex items-center gap-2 mt-2">
+                <span className="text-gray-600 text-sm font-medium">4.1</span>
+                {Array(5)
+                  .fill(0)
+                  .map((_, i) => (
+                    <StarIcon
+                      key={i}
+                      stroke="#FF5C00"
+                      fill="#FF5C00"
+                      size={16}
+                      className="mb-1"
+                    />
+                  ))}
+                <span className="text-gray-600 text-sm font-medium">1,234</span>
+              </div>
+            </div>
+          </div>
+        ))}
     </div>
   );
 }
