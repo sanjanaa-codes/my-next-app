d8789ac99acdc90909ecfe21ad8cf516a6f9e29b
R J Sanjanaa <sanjanaa0727@gmail.com>
2025-10-05 23:21:45 +0530

    Update productCard.tsx

--- app/components/productCard.tsx
+++ app/components/productCard.tsx
@@ -18,143 +18,111 @@ export default function ProductCard() {
     if (count > 1) {
       setCount((prev) => prev - 1);
     } else {
-      // If count is 1, show trash instead
       setShowControls(false);
       setCount(1);
     }
   };
+
   return (
     <div className="grid grid-cols-3 gap-8 px-12 mt-20">
       {Array(9)
         .fill(0)
         .map((_, i) => (
-          // why  key
           <div key={i}>
             <Image
-              key={i}
               src="/images/imagecardbanner.jpg"
               alt="Product Image"
               width={200}
               height={200}
               className="w-full h-auto cursor-pointer"
             />
+
             <div>
               <h5 className="text-base font-normal mt-2">RE Weather Jacket</h5>
-              {/* truncate */}
               <p className="text-gray-600 truncate text-base font-medium mt-1">
                 Prestige Omega Select Plus 27.5cm Non-Stick Omni Tawa |Induction
                 Base |Gas & Induction Compatible| Metal Spoon Friendly | Sturdy
                 Handles
               </p>
-              <div className="flex items-center gap-2 mt-2">
-                <span className="text-gray-600 text-sm font-medium">4.1</span>
-                {/* this method */}
-                {Array(5)
-                  .fill(0)
-                  .map((_, i) => (
-                    <StarIcon
-                      key={i}
-                      stroke="#FF5C00"
-                      fill="#FF5C00"
-                      size={16}
-                      className="mb-1"
-                    />
-                  ))}
-                <span className="text-gray-500 text-sm font-medium">1,234</span>
+
+              <div className="flex justify-between items-center">
+                <div className="flex items-center gap-2">
+                  <span className="text-gray-600 text-sm font-medium">4.1</span>
+                  {Array(5)
+                    .fill(0)
+                    .map((_, i) => (
+                      <StarIcon
+                        key={i}
+                        stroke="#FF5C00"
+                        fill="#FF5C00"
+                        size={16}
+                      />
+                    ))}
+                  <span className="text-gray-500 text-sm font-medium">
+                    1,234
+                  </span>
+                </div>
+                {/*  */}
+                <div>
+                  {/* Cart icon */}
+                  <button onClick={() => setShowControls((prev) => !prev)}>
+                    <Cart size={24} className="mt-1 cursor-pointer" />
+                  </button>
+
+                  {/* Controls */}
+                  {showControls && (
+                    <div className="flex items-center gap-4 mt-2">
+                      {count === 1 ? (
+                        <button onClick={() => setShowControls(false)}>
+                          <TrashIcon size={18} className="cursor-pointer" />
+                        </button>
+                      ) : (
+                        <Image
+                          src="/images/minus.png"
+                          alt="Minus Icon"
+                          width={80}
+                          height={20}
+                          className="h-5 w-auto cursor-pointer"
+                          onClick={handleMinus}
+                        />
+                      )}
+
+                      <p className="text-base font-semibold">{count}</p>
+
+                      <button onClick={handlePlus}>
+                        <Plus size={22} className="cursor-pointer" />
+                      </button>
+                    </div>
+                  )}
+                </div>
+                {/*  */}
               </div>
-              {/* items-baseline */}
+
               <div className="flex items-baseline space-x-2 mb-1">
                 <p className="text-xl font-semibold text-gray-900">
-                  {/* align-text-top align-super */}
                   <span className="text-xs align-text-top">₹</span>265
                   <span className="text-sm align-super">00</span>
                 </p>
                 <p className="text-sm text-gray-500">
-                  M.R.P: {/* {" "} */}
-                  {/* line-through */}
+                  M.R.P:{" "}
                   <span className="line-through text-gray-500">₹599</span>
                 </p>
               </div>
+
               <div className="flex gap-1">
                 <p className="text-sm text-gray-900 font-medium">
                   FREE Delivery
                 </p>
-                {/* <p className="text-sm font-semibold text-gray-900">Wed, 8 October</p> */}
                 <div className="space-y-2">
-                  {/* <DeliveryInfo type="single" date="Wed, 8 October" /> */}
+                  {/* <DeliveryInfo type="prime" /> */}
                   {/* <DeliveryInfo type="range" date="8" endDate="12 Oct" /> */}
-                  <DeliveryInfo type="prime" />
-                  {/* <DeliveryInfo
+                  {/* <DeliveryInfo type="single" date="Wed, 8 October" /> */}
+                   <DeliveryInfo
                     type="scheduled"
                     date="8 Oct"
                     time="8 am - 5 pm"
-                  /> */}
+                  />
                 </div>
-              </div>
-              {/* <Cart size={20} className="mt-5 cursor-pointer" />
-              <div className="flex items-center gap-4 mt-2">
-                <Image
-                  src="/images/minus.png"
-                  alt="Minus Icon"
-                  width={80}
-                  height={20}
-                  className="h-5 w-auto cursor-pointer"
-                />
-                <TrashIcon size={20} fill="#000" className="cursor-pointer" />
-
-                <Plus size={20} className="cursor-pointer" />
-              </div> */}
-              <div className="flex flex-col items-center">
-                {/* Cart icon */}
-                <button onClick={() => setShowControls((prev) => !prev)}>
-                <Cart
-                  size={24}
-                  className="mt-5 cursor-pointer"
-                />
-                </button>
-                
-
-                {/* Controls (appear when cart clicked) */}
-                {showControls && (
-                  <div className="flex items-center gap-4 mt-2">
-                    {count === 1 ? (
-                      <button onClick={() => setShowControls(false)}>
-                        <TrashIcon
-                          size={22}
-                          className="cursor-pointer"
-                          
-                        />
-                      </button>
-                    ) : (
-                      <Image
-                        src="/images/minus.png"
-                        alt="Minus Icon"
-                        width={80}
-                        height={20}
-                        className="h-5 w-auto cursor-pointer"
-                        onClick={handleMinus}
-                      />
-                    )}
-                    ) : (
-                      <Image
-                        src="/images/minus.png"
-                        alt="Minus Icon"
-                        width={80}
-                        height={20}
-                        className="h-5 w-auto cursor-pointer"
-                        onClick={handleMinus}
-                      />
-                    )}
-
-                    <p className="text-base font-semibold">{count}</p>
-                    <button onClick={handlePlus}><Plus
-                      size={22}
-                      className="cursor-pointer"
-                      
-                    /></button>
-                    
-                  </div>
-                )}
               </div>
             </div>
           </div>
