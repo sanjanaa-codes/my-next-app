e0a0dbb746b3a79754a2713e3f1cea8a9bdb0938
R J Sanjanaa <sanjanaa0727@gmail.com>
2025-10-05 20:37:24 +0530

    Update deliveryInfo.tsx

--- app/components/deliveryInfo.tsx
+++ app/components/deliveryInfo.tsx
@@ -1,22 +1,41 @@
+import Image from "next/image";
+
 type DeliveryProps = {
   type: "single" | "range" | "prime" | "scheduled";
   date?: string;
   endDate?: string;
   time?: string;
 };
-// Readonly why
-export default function DeliveryInfo({ type, date, endDate, time }: Readonly<DeliveryProps>) {
-  let message = "";
 
+export default function DeliveryInfo({
+  type,
+  date,
+  endDate,
+  time,
+}: Readonly<DeliveryProps>) {
+  // For "prime", we render an image, for others we render text
+  if (type === "prime") {
+    return (
+      <div>
+        <Image
+          src="/images/download.png"
+          alt="Assured Badge"
+          width={80}
+          height={20}
+          className="h-5 w-auto ml-1"
+        />
+      </div>
+    );
+  }
+
+  // For other types
+  let message = "";
   switch (type) {
     case "single":
       message = `${date}`;
       break;
     case "range":
       message = `${date} - ${endDate}`;
-      break;
-    case "prime":
-      message = "for Prime members";
       break;
     case "scheduled":
       message = `as soon as ${date}, ${time}`;
