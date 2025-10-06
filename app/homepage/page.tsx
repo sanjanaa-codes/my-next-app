7d8932a5726690857a496b941cf7d3621220f950
R J Sanjanaa <sanjanaa0727@gmail.com>
2025-10-06 12:29:43 +0530

    Update page.tsx

--- app/homepage/page.tsx
+++ app/homepage/page.tsx
@@ -1,9 +1,22 @@
+"use client";
+import { useState } from "react";
 import ProductCard from "../components/productCard";
 
 export default function HomePage() {
+  const [name, setName] = useState("Sanjanaa");
+  function handleClick() {
+    setName("R J Sanjanaa");
+  }
   return (
     <>
       <ProductCard />
+      <h3 className="mt-2">
+        {" "}
+        I am{" "}
+        <button onClick={handleClick} className="cursor-pointer">
+          {name}
+        </button>
+      </h3>
     </>
   );
 }
